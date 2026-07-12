563c1284879f332b29128e8ff6c88382addf86a3
Sören Köhler <soerenkoehler@users.noreply.github.com>
2026-07-13 01:00:25 +0200

    cleanup

--- deployment/20-storage.tf
+++ deployment/20-storage.tf
@@ -13,13 +13,7 @@ resource "azurerm_storage_account" "sharepass" {
 
   min_tls_version = "TLS1_2"
 
-  # Disables authentication via shared access keys and SAS tokens
-  # derived from them, enforcing Entra ID authentication for the
-  # data plane.
   shared_access_key_enabled = false
-
-  # Disallows public network access; adjust according to network
-  # requirements (private endpoints, service endpoints, etc.).
   public_network_access_enabled = true
 }
 
@@ -30,43 +24,4 @@ resource "azurerm_storage_account" "sharepass" {
 resource "azurerm_storage_queue" "sharepass" {
   name               = "sharepass-queue"
   storage_account_id = azurerm_storage_account.sharepass.id
-
-  # Note: with shared_access_key_enabled = false, Terraform itself
-  # must authenticate to the data plane via Entra ID. This requires
-  # the identity running Terraform (user, service principal, or
-  # managed identity) to hold an appropriate RBAC role, such as
-  # "Storage Queue Data Contributor", on the storage account.
-}
-
-# --------------------------------------------------------------------------
-# Role assignment for a consuming identity (application, user, or
-# managed identity) that will read/write messages on the queue.
-# Replace principal_id with the object ID of the relevant identity.
-# --------------------------------------------------------------------------
-
-variable "queue_consumer_principal_id" {
-  description = "Object ID of the Entra ID principal (user, group, service principal, or managed identity) that will access the queue."
-  type        = string
-}
-
-resource "azurerm_role_assignment" "queue_data_contributor" {
-  scope                = azurerm_storage_account.sharepass.id
-  role_definition_name = "Storage Queue Data Contributor"
-  principal_id         = var.queue_consumer_principal_id
-}
-
-# --------------------------------------------------------------------------
-# Outputs
-# --------------------------------------------------------------------------
-
-output "storage_account_name" {
-  value = azurerm_storage_account.sharepass.name
-}
-
-output "queue_name" {
-  value = azurerm_storage_queue.sharepass.name
-}
-
-output "queue_endpoint" {
-  value = "${azurerm_storage_account.sharepass.primary_queue_endpoint}${azurerm_storage_queue.sharepass.name}"
 }
