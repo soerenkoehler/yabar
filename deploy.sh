7c06692ea9fb35b80662df2140a261dbbf695499
Sören Köhler <soerenkoehler@users.noreply.github.com>
2026-07-20 15:43:33 +0200

    debug function deployment 7

---
+++ deploy.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -euo pipefail
+
+DEPLOY_ENV="${DEPLOY_ENV:-preview}"
+DEPLOYMENT_TOKEN=$(
+    tofu output -json 2>/dev/null \
+    | jq -r '.swa_deployment_token.value // ""' \
+    || true
+)
+
+if [[ -z "${DEPLOYMENT_TOKEN:-}" ]]; then
+    printf "\033[93mread deployment token from azure\033[0m\n"
+    DEPLOYMENT_TOKEN=$(
+        az staticwebapp secrets list --name "$PROJECT_PREFIX" \
+        | jq -r '.properties.apiKey'
+    )
+else
+    printf "\033[93muse deployment token from tofu\033[0m\n"
+fi
+
+docker run --rm \
+    -v "$PWD/../swa:/workspace" \
+    ghcr.io/soerenkoehler-org/docker-swacli:main \
+    deploy \
+        --verbose silly \
+        --env "$DEPLOY_ENV" \
+        --deployment-token "$DEPLOYMENT_TOKEN" \
