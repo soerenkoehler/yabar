24d03522bec2248ab7762cf29e562af28fcf792e
Sören Köhler <soerenkoehler@users.noreply.github.com>
2026-07-21 19:44:10 +0200

    debug function deployment 9

--- tf/40-function.tf
+++ tf/40-function.tf
@@ -35,7 +35,7 @@ resource "azurerm_function_app_flex_consumption" "functionApps" {
 
 resource "azurerm_role_assignment" "storage_roleassignment" {
   scope = azurerm_storage_account.sharepass.id
-  role_definition_name = "Storage Blob Data Contributor"
+  role_definition_name = "Storage Blob Data Owner"
   principal_id = azurerm_function_app_flex_consumption.functionApps.identity.0.principal_id
   principal_type = "ServicePrincipal"
 }
