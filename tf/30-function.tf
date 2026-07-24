0f9d4a78f4a1e6b58390d011a1643ad7115b963b
Sören Köhler <soerenkoehler@users.noreply.github.com>
2026-07-24 11:34:48 +0200

    test func core

--- tf/30-function.tf
+++ tf/30-function.tf
@@ -63,6 +63,7 @@ resource "azurerm_function_app_flex_consumption" "api" {
   }
 
   app_settings = {
+    "AzureWebJobsStorage"              = ""
     "AzureWebJobsStorage__accountName" = azurerm_storage_account.sharepass.name
     "AUTH_GOOGLE_CLIENT_ID"            = var.auth_google_client_id
   }
