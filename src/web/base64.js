a1d0e21c37e1d81b35ba8a5b316c0a0edf25c5ba
Sören Köhler <soerenkoehler@users.noreply.github.com>
2026-08-06 00:58:10 +0200

    fix base64 handling

--- src/web/base64.js
+++ src/web/base64.js
@@ -7,11 +7,12 @@ const SALT_LENGTH = 3;
 export const saltedBase64ToBytes = (s) => Uint8Array.fromBase64(s, BASE64URL).slice(SALT_LENGTH);
 export const saltedBase64ToString = (s) => new TextDecoder().decode(saltedBase64ToBytes(s));
 export const bytesToSaltedBase64 = (b) => {
+    const bytes = b instanceof Uint8Array ? b : new Uint8Array(b);
     const salt = new Uint8Array(SALT_LENGTH);
     crypto.getRandomValues(salt);
-    const combined = new Uint8Array(salt.length + b.length);
+    const combined = new Uint8Array(salt.length + bytes.length);
     combined.set(salt);
-    combined.set(b, salt.length);
+    combined.set(bytes, salt.length);
     return combined.toBase64(BASE64URL);
 }
 export const stringToSaltedBase64 = (s) => bytesToSaltedBase64(new TextEncoder().encode(s));
